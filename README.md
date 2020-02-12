505e6932de852b3d2c78fe7c3ae6cea0c9a8dbb0
crf61 <crf61@georgetown.edu>
2020-02-12 17:39:16 -0500

    Issuing requests

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # web-requests
+
+# set-up virtual environment
+    After cloning the readme and creating activating conda, install the requests package
--- get_data.py
+++ get_data.py
@@ -1,1 +1,16 @@
+import requests
+
+
 print("REQUESTING SOME DATA FROM THE INTERNET...")
+
+
+request_url = "https://raw.githubusercontent.com/prof-rossetti/intro-to-python/master/data/products/1.json"
+print ("URL:" request_url)
+
+
+response = requests.get(request_url)
+print(type(response))
+print(dir(response))
+
+print(response.status_code)
+print(response.text)
